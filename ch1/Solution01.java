289343120f097faa6e62d67efe7af244f478dedc
xiayuchen <ahcxy2010@gmail.com>
2015-09-20 02:33:52 +0800

    Update Solution01.java

--- ch1/Solution01.java
+++ ch1/Solution01.java
@@ -1,4 +1,5 @@
 public class Solution01{
+	//we use a constant array for storing the appearance of each ascii character, the code will take O(1) space since the length of s should be no longer than 256, it also takes O(1) time
 	public static boolean if_all_unique(String s){
 		if(s.length()>256)
 			return false;
