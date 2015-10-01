024ebca026b2dbf9d5dc82fca77352818472cf9e
xiayuchen <ahcxy2010@gmail.com>
2015-10-01 21:07:04 +0800

    Create Solution06.java

---
+++ ch10/Solution06.java
@@ -0,0 +1,5 @@
+public class Solution06{
+	//To sort a file of 20GB size, we should use external sort, every time the program read in string of size X, where X is the size of the memory
+	//We sort the read string and save them back to the file system. And then we merge each sorted chunk one by one.
+
+}
