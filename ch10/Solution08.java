0f9bd4ece2fa6f2228edddfb02b1073ce3a53d2c
xiayuchen <ahcxy2010@gmail.com>
2015-10-01 22:20:24 +0800

    Update Solution08.java

--- ch10/Solution08.java
+++ ch10/Solution08.java
@@ -1,33 +1,30 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
 
-public class Solution07{
-	//In the solution, we create a bit array for all the possible integer, if the integer has shown in the file, we set the bit in corresponding 
-	//position to 1, then we scan the array for the position where the value is 0
-	//This solution scan runs in O(n) time
-	//Follow up: If we only has 10MB memory, we need to cut the numbers into 1000 blocks, first we calculate the numbers that falls in each blocks,
-	//And then we choose the blocks that is not full, and create a bit array for numbers in this range, scan through all the number to find the number
-	//missing
-	//Using this way, we can through the file for two times, runtime complexity is O(n)
-	public static int find_missing() throws FileNotFoundException{
-		Scanner sc=new Scanner(new FileReader("input.txt"));
-		long numberofint=(Integer.MAX_VALUE)+1;
-		byte[] visited_num=new byte[(int)(numberofint/8)];
-		while(sc.hasNextInt()){
-			int n=sc.nextInt();
-			int index=n/8;
-			int index_bit=n%8;
-			visited_num[index]|=1<<index_bit;
+public class Solution08{
+	//With 4 kilobytes, we have approximately 32k bits, for the number is from 1 to N where N<32000, so we could create a bitset to storing if
+	//a number has shown or not
+	//In this solution, we assume that if a duplicate has shown 3 times in the array, we should print out it twice, that means we do not 
+	//store if we have already printed out a duplicate
+	//This alogithm runs in O(n) time, and only use the 4 kilobytes
+	public static void duplicate(int [] n){
+		BitSet store= new BitSet(32000);
+		for(int i:n){
+			if(store.get(i-1))
+				System.out.println(i);
+			else
+				store.set(i-1);
 		}
-		int result=0;
-		for(int i=0;i<=Integer.MAX_VALUE;i++){
-			int index=(int)(i/8);
-			int index_bit=(int)(i%8);
-			if((visited_num[index] & 1<<index_bit) == 0)
-				result=i;
-				return result;
+		return;
+	}
+	public static void main(String [] args){
+		int []nums= new int [32000];
+		for(int i=0;i<31998;i++){
+			nums[i]=i+1;
 		}
-		return result;
+		nums[31998]=3;
+		nums[31999]=5;
+		duplicate(nums);
 	}
 }
